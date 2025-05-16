67a28243805cfb3da5911df7147c664260539766
DrDehydratedWater <karpadf@gmail.com>
2025-05-16 21:32:06 +0200

    eye

--- resources/js/index.js
+++ resources/js/index.js
@@ -13,12 +13,15 @@ function invert_toggle() {
 
 function title_container_lift() {
     const titleContainer = document.getElementById("title-container");
+    const eye = document.getElementById("eye");
     
     if (title_container_lift_toggle === false) {
         titleContainer.classList.add('clicked');
+        eye.classList.add('rotate');
         title_container_lift_toggle = true;
     } else {
         titleContainer.classList.remove('clicked');
+        eye.classList.add('rotate');
         title_container_lift_toggle = false;
     }
 }
